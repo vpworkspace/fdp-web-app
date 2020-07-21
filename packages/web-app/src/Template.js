f189f3126e9540b2a2963eac4db53988689700fa
Jag <jagmohan.bhanot@gmail.com>
2020-07-21 16:14:37 +0530

    Moved Scheduled Payments from /fdp to /template

--- packages/web-app/src/Template.js
+++ packages/web-app/src/Template.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-import DoughnutChart from "./views/DoughnutChart";
-import './App.css';
+import React, { Component } from 'react'
+import DoughnutChart from './views/DoughnutChart'
+import './App.css'
+import SchduledPayments from './SchduledPayments'
+import Grid from '@material-ui/core/Grid'
+
 class Template extends Component {
-  
-  render() {    
-    return (
-		<div class="medbox">
-      <DoughnutChart/>
-		</div>
-    );
-  }
+    render() {
+        return (
+            <Grid container spacing={3}>
+                <Grid item xs={6} spacing={3}>
+                    <SchduledPayments />
+                </Grid>
+                <Grid item xs={6} spacing={3}>
+                    <DoughnutChart />
+                </Grid>
+            </Grid>
+        )
+    }
 }
 
-export default Template;
+export default Template
